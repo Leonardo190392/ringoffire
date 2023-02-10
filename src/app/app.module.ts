ffd18a6ac40b2e862dff87d4f3ecc6c6d430b4a8
Leonardo van Dahl <111518533+Leonardo190392@users.noreply.github.com>
2023-02-10 11:20:37 +0100

    update responsive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,6 +26,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { provideDatabase,getDatabase } from '@angular/fire/database';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { PlayerMobileComponent } from './player-mobile/player-mobile.component';
 
 
 
@@ -37,6 +38,7 @@ import { provideFirestore,getFirestore } from '@angular/fire/firestore';
     PlayerComponent,
     DialogAddPlayerComponent,
     GameInfoComponent,
+    PlayerMobileComponent,
     
   ],
   imports: [
